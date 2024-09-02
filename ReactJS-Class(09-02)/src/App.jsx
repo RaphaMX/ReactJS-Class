a252fce7f84de2c03e370c2bc9e1206c62b05478
RaphaMX <161722373+RaphaMX@users.noreply.github.com>
2024-09-02 17:52:22 +0000

    add click

--- ReactJS-Class(09-02)/src/App.jsx
+++ ReactJS-Class(09-02)/src/App.jsx
@@ -1,25 +1,13 @@
 import './App.css'
 
-const students = [
-  {name: "Margit Omen", courses: ["ADM", "ECO"]},
-  {name: "Rennala Fullmoon", courses: ["GSRC", "MOS"]},
-]
+function sayHi() {
+  alert("こんにちは、苦い")
+}
 
 function App() {
   return (
     <>
-      {
-        students.map((std, i) => (
-          <div key={i}>
-            <h2>{std.name}</h2>
-            {
-              std.courses.map((crs, i) => (
-                <h3 key={i}> {crs} </h3>
-              ))
-            }
-        </div>
-        ))
-      }
+      <button onClick={sayHi}>Click Here</button>
     </>
   )
 }
