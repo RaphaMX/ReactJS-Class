148dcc586bf06e8eeac1c7e6c5327616c468ef18
RaphaMX <161722373+RaphaMX@users.noreply.github.com>
2024-09-02 18:10:59 +0000

    change to default

--- ReactJS-Class(09-02)/src/App.jsx
+++ ReactJS-Class(09-02)/src/App.jsx
@@ -1,25 +1,8 @@
 import './App.css'
 
-const students = [
-  {name: "Margit Omen", courses: ["ADM", "ECO"]},
-  {name: "Rennala Fullmoon", courses: ["GSRC", "MOS"]},
-]
-
 function App() {
   return (
     <>
-      {
-        students.map((std, i) => (
-          <div key={i}>
-            <h2>{std.name}</h2>
-            {
-              std.courses.map((crs, i) => (
-                <h3 key={i}> {crs} </h3>
-              ))
-            }
-        </div>
-        ))
-      }
     </>
   )
 }
